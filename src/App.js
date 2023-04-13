123ef38c291e23b3045e6ca6c03e46874269dffd
Manoel Felipe <mftneto@gmail.com>
2023-04-13 19:01:41 -0300

    Criando a entrada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 
-const App = () => <h1 className='title'>App</h1>
+const App = () => {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
 
+  const [value, setValue] = useState("");
+
+  const submit = () => {
+    console.log("submit", value);
+    erase();
+  }
+
+  const onChange = (event) =>{
+    setValue(event.target.value);
+  }
+
+  const erase = () => {
+    setValue("");
+  }
+
+  const onKeyDown = (event) =>{
+    if(event.which === ENTER_KEY){
+      submit();
+    } else if(event.which === ESCAPE_KEY){
+      erase()
+    }
+  }
+  return (
+    <section id='app' className='container'>
+      <header>
+        <h1 className='title'>Todo</h1>
+      </header>
+      <section className='main'>
+        <input
+        className='new-todo'
+        placeholder='o que precisa ser feito ?'
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        />
+      </section>
+    </section>
+  );
+}
 
 export default App;
