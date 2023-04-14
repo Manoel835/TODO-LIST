65eb6ff859266c79f985f9509310b6ad35e6f218
Manoel Felipe <mftneto@gmail.com>
2023-04-14 18:28:32 -0300

    Lista

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import React, {useState} from 'react';
+import {TiDelete} from 'react-icons/ti'
 
 const App = () => {
   const ESCAPE_KEY = 27;
   const ENTER_KEY = 13;
 
+const initialTodos= [
+  { id:1, title: "Estudar React", checked: false},
+  { id:2, title: "Estudar inglês", checked: true},
+  { id:3, title: "Tocar guitarra", checked: false},
+];
+
+  const [todos, ] = useState(initialTodos);
   const [value, setValue] = useState("");
 
   const submit = () => {
@@ -40,6 +48,18 @@ const App = () => {
         onChange={onChange}
         onKeyDown={onKeyDown}
         />
+        <ul className='todo-list'>
+          {
+            todos.map((todo) =>(
+              <li key={todo.id.toString()}>
+                <span className='todo'>{todo.title}</span>
+                <button className='remove'>
+                  <TiDelete size={28}/>
+                </button>
+              </li>
+            ))
+          }
+        </ul>
       </section>
     </section>
   );
