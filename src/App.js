de950b4421e70c9251e07b06bc89fd4e8624c3a5
Manoel Felipe <mftneto@gmail.com>
2023-04-14 23:17:04 -0300

    task completed

--- src/App.js
+++ src/App.js
@@ -1,63 +1,78 @@
-import './App.css';
-import React, {useState} from 'react';
-import {TiDelete} from 'react-icons/ti'
+import "./App.css";
+import React, { useState } from "react";
+import { TiDelete } from "react-icons/ti";
 
 const App = () => {
   const ESCAPE_KEY = 27;
   const ENTER_KEY = 13;
 
-
-
-  const [todos, setTodos ] = useState([]);
+  const [todos, setTodos] = useState([]);
   const [value, setValue] = useState("");
 
   const erase = () => {
     setValue("");
-  }
+  };
+
   const submit = () => {
-    setTodos([...todos, { id:new Date().getTime(), title: value, checked: false}])
+    setTodos([
+      ...todos,
+      { id: new Date().getTime(), title: value, checked: false },
+    ]);
     erase();
-  }
+  };
 
-  const onChange = (event) =>{
+  const onChange = (event) => {
     setValue(event.target.value);
-  }
+  };
 
-  const onKeyDown = (event) =>{
-    if(event.which === ENTER_KEY){
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
       submit();
-    } else if(event.which === ESCAPE_KEY){
-      erase()
+    } else if (event.which === ESCAPE_KEY) {
+      erase();
     }
-  }
+  };
+
+  const onToggle = (todo) => {
+    setTodos(
+      todos.map((obj) =>
+        obj.id === todo.id ? { ...obj, checked: !todo.checked } : obj
+      )
+    );
+  };
   return (
-    <section id='app' className='container'>
+    <section id="app" className="container">
       <header>
-        <h1 className='title'>Todo</h1>
+        <h1 className="title">Todo</h1>
       </header>
-      <section className='main'>
+      <section className="main">
         <input
-        className='new-todo'
-        placeholder='o que precisa ser feito ?'
-        value={value}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
+          className="new-todo"
+          placeholder="o que precisa ser feito ?"
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
         />
-        <ul className='todo-list'>
-          {
-            todos.map((todo) =>(
-              <li key={todo.id.toString()}>
-                <span className='todo'>{todo.title}</span>
-                <button className='remove'>
-                  <TiDelete size={28}/>
-                </button>
-              </li>
-            ))
-          }
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id.toString()}>
+              <span
+                className={["todo", todo.checked ? "checked" : ""].join(" ")}
+                onClick={() => onToggle(todo)}
+                role="button"
+                tabIndex={0}
+              >
+                {todo.title}
+              </span>
+              <button className="remove">
+                <TiDelete size={28} />
+              </button>
+            </li>
+          ))}
         </ul>
       </section>
     </section>
   );
-}
+};
 
 export default App;
